b03f30be9f21cb1e68b6b7cd9cd5d81729cbb6f8
anna-tsang <94375870+anna-tsang@users.noreply.github.com>
2021-11-19 22:12:20 +0800

    Update main.js

--- 2.function2/main.js
+++ 2.function2/main.js
@@ -1,9 +1,17 @@
-function reverseString(message){
+function palindrome(message){
     // wirte your code here
     var newString = "";
-    for (var i = message.length - 1; i >= 0; i--) {
-        newString += message[i];
-    }
-    console.log(newString);
+      for (var i = message.length - 1; i >= 0; i--) {
+        for (var j = 0; j < message.length; j++){
+          if(message[i] == message [j]){
+          } else {
+            console.log(false);
+            return false;
+          }
+          console.log(true);
+          return true;
+        }
+      }
   }
-  reverseString('hello'); // should return 'olleh'
+  palindrome('hello'); // should return false
+  palindrome('abcba'); // should return true
